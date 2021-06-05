e9730873f64c8081029a732c0135f1035fc9acd6
Sam Kohlbecher <luisa.kohlbecher@ue-germany.de>
2021-06-05 15:39:20 +0200

    [ADDED] Options Scene and a Volume Slider
    The Volume Slider is there for the Play to decide how load they want the sounds to be. The Slider does not work. The Problem is, yet again, a problem regarding the new Input System and does not work as of now

---
+++ Assets/_GAMEASSETS/Scripts/SetVolume.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio; //required to acces Audio Mixer functions
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer mixer; 
+
+    public void SetLevel (float sliderValue) //needs to be public to be exposed in the "on value chagned" event of the VolumeSlider
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+}
